ae889f8b6301ced51c49ed0e4a79f9f3044c0304
Rishikesh <Rishikesh@gmail.com>
2025-07-24 12:35:30 +0530

    voter searching list

--- online_voter/src/App.jsx
+++ online_voter/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
+import SearchVoterList from './components/search-voter-list'
+
 
 function App() {
  
 
   return (
     <>
-      
+      <SearchVoterList />
     </>
   )
 }
